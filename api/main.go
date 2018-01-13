e711dd306089afe41ed4ec75564a3602ac1d6809
Duan Hao <haoduan@wifi-172-23-122-82.uoa-wifi.auckland.ac.nz>
2018-01-13 15:32:20 +1300

    ###added - structure of an empty service

---
+++ api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/nats-io/go-nats"
+	"github.com/nzgogo/micro"
+)
+
+func main() {
+	srv := gogo.NewService(
+		"gogox.core.api",
+		"v1",
+	)
+
+	srv.Options().Transport.SetHandler(func(msg *nats.Msg) {
+
+	})
+
+	srv.Init()
+
+	srv.Run()
+}
